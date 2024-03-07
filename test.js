7e7d30a2d0bdb8689716d20c83c2ebad101d2f1d
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2024-03-07 23:20:31 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log('test');
+
+let boolean = true;
+
+if (boolean) {
+  const aaa = 'arr';
+  console.log(aaa);
+}
+const hell_Workd = 'afda';
+console.log(hellWorkd);
+const a = 'test';
+
+console.log(a);
